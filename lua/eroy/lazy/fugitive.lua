a536893fc7145df4e71cd7659626f3d9b2e087a1
Eric Roy <eric.roy@knak.com>
2024-05-31 09:52:28 -0400

    Ripped out nonsense hack window/buffer recovery I added in fugitive, fixed basic fugitive commands

--- lua/eroy/lazy/fugitive.lua
+++ lua/eroy/lazy/fugitive.lua
@@ -1,70 +1,9 @@
 return {
     "tpope/vim-fugitive",
     config = function()
-        -- Track the previous buffer and Fugitive status window
-        _G.previous_bufnr = nil
-        _G.fugitive_status_winid = nil
-
-        -- Function to open Fugitive status and track previous buffer
-        function OpenFugitive()
-            local current_buf = vim.api.nvim_get_current_buf()
-            local bufname = vim.api.nvim_buf_get_name(current_buf)
-            if bufname and bufname ~= "" then
-                vim.cmd("mkview 1")
-            end
-            _G.previous_bufnr = current_buf
-            vim.cmd.Git()
-            _G.fugitive_status_winid = vim.api.nvim_get_current_win()
-        end
-
--- Function to close the diff view and return to the Fugitive status window
-function CloseDiffAndReturn()
-    if vim.bo.modified then
-        local bufname = vim.api.nvim_buf_get_name(0)
-        if bufname and bufname ~= "" then
-            vim.cmd("silent! write!")
-        else
-            vim.cmd("silent! edit!")
-        end
-    end
-    vim.cmd("q!")
-    if _G.fugitive_status_winid and vim.api.nvim_win_is_valid(_G.fugitive_status_winid) then
-        vim.api.nvim_set_current_win(_G.fugitive_status_winid)
-    end
-end
-
--- Function to close Fugitive status window and return to the previous buffer with view restored
-function CloseFugitiveAndReturn()
-    if vim.bo.modified then
-        local bufname = vim.api.nvim_buf_get_name(0)
-        if bufname and bufname ~= "" then
-            vim.cmd("silent! write!")
-        else
-            vim.cmd("silent! edit!")
-        end
-    end
-    vim.cmd("q!")
-    if _G.previous_bufnr and vim.api.nvim_buf_is_valid(_G.previous_bufnr) then
-        local bufname = vim.api.nvim_buf_get_name(_G.previous_bufnr)
-        if bufname and bufname ~= "" then
-            vim.cmd("buffer " .. _G.previous_bufnr)
-            vim.cmd("loadview 1")
-        else
-            vim.cmd("buffer " .. _G.previous_bufnr)
-        end
-    end
-end
-
         -- Map '<leader>gs' to open Fugitive status window and track previous buffer
-        vim.keymap.set("n", "<leader>gs", ":lua OpenFugitive()<CR>", { noremap = true, silent = true })
-        vim.keymap.set("n", "<leader>gb", ":Git blame", { noremap = true, silent = true })
-        vim.keymap.set("n", "<Esc>", function()
-            if vim.bo.ft == "fugitive" then
-                CloseFugitiveAndReturn()
-            else
-                CloseDiffAndReturn()
-            end
-        end, { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>gs", ":Git <CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>gb", ":Git blame <CR>", { noremap = true, silent = true })
 
         -- Create autocommand group for Fugitive
         local Eroy_Fugitive = vim.api.nvim_create_augroup("eroy_fugitive", {})
@@ -90,18 +29,6 @@ end
             group = Eroy_Fugitive,
             pattern = "*",
             callback = function()
-                if vim.wo.diff then
-                    local bufnr = vim.api.nvim_get_current_buf()
-                    -- Key mappings for diff view
-                    vim.keymap.set("n", "<leader>gU", ":Git checkout %<CR>",
-                        { noremap = true, silent = true, buffer = bufnr })
-                    vim.keymap.set("n", "<leader>gu", ":Git checkout -p<CR>",
-                        { noremap = true, silent = true, buffer = bufnr })
-                end
-                if vim.bo.ft ~= "fugitive" then
-                    return
-                end
-
                 local bufnr = vim.api.nvim_get_current_buf()
                 local opts = { buffer = bufnr, remap = false }
                 vim.keymap.set("n", "<leader>p", function()
--- lua/eroy/remap.lua
+++ lua/eroy/remap.lua
@@ -30,7 +30,6 @@ To yank whole page, ggVG then leader y
 --]]
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
@@ -47,7 +46,6 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
 vim.keymap.set(
     "n",
     "<leader>ee",
