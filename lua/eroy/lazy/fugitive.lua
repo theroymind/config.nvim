6a5e62cda45dcbbb427e5b319c8cd88382e5d202
Eric Roy <eric.roy@knak.com>
2024-07-31 15:49:38 -0400

    Added highlight colors

--- lua/eroy/lazy/fugitive.lua
+++ lua/eroy/lazy/fugitive.lua
@@ -29,14 +29,6 @@ return {
             pattern = "*",
             callback = function()
                 local bufnr = vim.api.nvim_get_current_buf()
-                local opts = { buffer = bufnr, remap = false }
-                vim.keymap.set("n", "<leader>p", function()
-                    vim.cmd.Git('push')
-                end, opts)
-
-                vim.keymap.set("n", "<leader>P", function()
-                    vim.cmd.Git({ 'pull', '--rebase' })
-                end, opts)
 
                 vim.keymap.set("n", "<leader>gU", ":Git checkout %<CR>",
                     { noremap = true, silent = true, buffer = bufnr })
